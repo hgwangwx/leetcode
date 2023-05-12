b0d63937931ad23d6291a58226cdbd11d02d8a74
wangwenxuan1 <wangwenxuan1@sensetime.com>
2023-05-12 18:11:00 +0800

    add fun

---
+++ memcpy实现.cpp
@@ -0,0 +1,31 @@
+void *memcpy(void *dst, const void *src, size_t size)
+{
+    char *psrc;
+    char *pdst;
+
+    if (nullptr == dst || nullptr == src)
+    {
+        return nullptr;
+    }
+
+    if ((src < dst) && (char *)src + size > (char *)dst) // 出现地址重叠的情况，自后向前拷贝
+    {
+        psrc = (char *)src + size - 1;
+        pdst = (char *)dst + size - 1;
+        while (size--)
+        {
+            *pdst-- = *psrc--;
+        }
+    }
+    else
+    {
+        psrc = (char *)src;
+        pdst = (char *)dst;
+        while (size--)
+        {
+            *pdst++ = *psrc++;
+        }
+    }
+
+    return dst;
+}
---
+++ 智能指针实现.cpp
@@ -0,0 +1,54 @@
+template<typename T>
+class SmartPtr {
+public:
+    SmartPtr(): ptr(nullptr), ref_count(nullptr) {}
+    SmartPtr(T* t): ptr(t), ref_count(new int(1)){}
+    SmartPtr(SmartPtr& other) {
+        ptr = other.ptr;
+        ref_count = other.ref_count;
+        (*ref_count)++;
+    }
+
+    // 析构函数
+    ~SmartPtr() {
+        if(0 == --(*ref_count))
+        {
+            delete ptr;
+            delete ref_count;
+            ptr = nullptr;
+            ref_count = nullptr;
+        }
+    }
+
+    SmartPtr<T> operator=(const SmartPtr<T>& other)
+    {
+        // 自我赋值
+        if (this != &sp) {
+            // 当前指针引用计数减一
+            if (--(*ref_count) == 0) {
+                delete ptr;
+                delete ref_count;
+            }
+
+        ptr = other.ptr;
+        ref_count = other.ref_count;
+        (*ref_count)++;
+    }
+
+    // 解引用运算符
+    T& operator*() const {
+        assert(this->_ptr == nullptr);
+        return *(this->ptr);
+    }
+
+    // 指针运算符
+    T* operator->() const {
+        assert(this->_ptr == nullptr);
+        return this->ptr;
+    }
+
+private:
+    T* ptr;
+    int* ref_count;
+
+};
